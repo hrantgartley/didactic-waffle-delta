46978623683f44ca7e1fdb0cf69bb21ba8b82c63
hrantgartley <doyouevenjump@gmail.com>
2024-02-10 18:42:30 -0600

    finalish

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,68 @@
 /* global $ */
-$(function () {
-    $("#fact-button").on("click", function () {
-        console.log("button has been clicked")
-    })
+$("#fact-button").on("click", function () {
+    displayFact()
 })
+
+function getLength() {
+    let text = $("#name-input").val()
+    if (text !== undefined) {
+        let count = 0
+        for (let i = 0; i < text.length; i++) {
+            if (text[i] !== " ") {
+                count++
+            }
+        }
+        return count
+    } else {
+        return 0
+    }
+}
+
+function getSpiritAnimal() {
+    let text = $("#name-input").val()
+    if (text !== undefined) {
+        let animals = [
+            "Cat",
+            "Dog",
+            "Elephant",
+            "Lion",
+            "Tiger",
+            "Bear",
+            "Wolf",
+            "Fox",
+            "Horse",
+            "Rabbit",
+        ]
+        let index = text.length % animals.length
+        return animals[index]
+    } else {
+        return "No name"
+    }
+}
+
+function displayFact() {
+    let text = $("#name-input").val()
+    // change fact to template string
+    let fact = `${text} is ${getLength()} characters long.`
+    $("#length").text(fact)
+    $("#animal").text(`Your spirit animal is: ${getSpiritAnimal()}`)
+    $("#fact").text(`Random Fact:  ${randomFacts()}`)
+}
+
+function randomFacts() {
+    let facts = [
+        "Ants stretch when they wake up in the morning.",
+        "Ostriches can run faster than horses.",
+        "Olympic gold medals are actually made mostly of silver.",
+        "You are born with 300 bones; by the time you are an adult you will have 206.",
+        "It takes about 8 minutes for light from the Sun to reach Earth.",
+        "Some bamboo plants can grow almost a meter in just one day.",
+        "The state of Florida is bigger than England.",
+        "Some penguins can leap 2-3 meters out of the water.",
+        "On average, it takes 66 days to form a new habit.",
+        "Mammoths still walked the Earth when the Great Pyramid was being built.",
+        "Rabbit's teeth never stop growing.",
+    ]
+    let index = Math.floor(Math.random() * facts.length)
+    return facts[index]
+}
